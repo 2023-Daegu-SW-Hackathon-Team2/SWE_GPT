07d32a47b485626b9218811c0c0738f2daf97583
re1yun <re1yun@knu.ac.kr>
2023-11-11 16:17:35 +0900

    Add Deepl API integration for translating marketing slogans to Korean

--- gpt.py
+++ gpt.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from gpt4all import GPT4All
+import deepl
 
 app = Flask(__name__)
+# deepl api key
+auth_key = "."
+translator = deepl.Translator(auth_key)
+
+def store_sentences(marketing_slogans):
+    sentences = marketing_slogans.split("\"")
+    return [sentence.strip() for sentence in sentences if len(sentence.strip()) >= 5 and not sentence.strip().isdigit()]
 
 @app.route('/')
 def hello_world():
@@ -18,7 +26,16 @@ def shopt():
     Product_Benefits = "comfortable, durable, stylish"
     prompt = f"AI, could you please create multiple marketing slogans for our product named '{Product_Name}'? The product is '{Product_Description}', and its price is '{Product_Price}'. The main benefits of this product are '{Product_Benefits}'.\n- AI:(Generates Multiple Marketing Slogan)"
     output = model.generate(prompt)
-    return output
+    print(output)
+    stripped_sentences = store_sentences(output)
+    all_sentences = translator.translate_text(stripped_sentences, target_lang = "KO")
+
+    ko_output = []
+    for sentence in all_sentences:
+        ko_output.append(sentence.text)
+    """ for sentence in result:
+        ko_output.append(translator.translate_text(sentence, target_lang = "KO").text) """
+    return jsonify(ko_output)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
